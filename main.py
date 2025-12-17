d584e3780e7a14b65a1950aad30c0a060c4c5661
Yasir Ali <iamyasirali646@gmail.com>
2025-12-17 13:37:40 +0500

    feat: initial commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from .models.post_model import Post
 from .models.user_model import User
-from database import Base, engine
+from .database import Base, engine
 from .routes.user_route import router as user_router
 from .routes.post_route import router as post_router
 
