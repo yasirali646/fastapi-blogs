2ce42a6a13f45ae45a41fd0e8c6fe4d95b2774c8
Yasir Ali <iamyasirali646@gmail.com>
2025-12-17 13:36:10 +0500

    feat: initial commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from models.post_model import Post
-from models.user_model import User
+from .models.post_model import Post
+from .models.user_model import User
 from database import Base, engine
-from routes.user_route import router as user_router
-from routes.post_route import router as post_router
+from .routes.user_route import router as user_router
+from .routes.post_route import router as post_router
 
 
 Base.metadata.create_all(bind=engine)
